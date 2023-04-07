71e76622ab1215d2a15c0c849ca5ddad1305fff3
sAyflort <neefe.saratan.life.life@gmail.com>
2023-04-07 11:39:41 +0300

    Написан подсчет интеграла френеля

--- src/main/java/ru/shihov/forsunki/fors/HelloController.java
+++ src/main/java/ru/shihov/forsunki/fors/HelloController.java
@@ -2,13 +2,24 @@ package ru.shihov.forsunki.fors;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField textField;
+
+    @FXML
+    private TextField epsilon;
+
+    @FXML
+    private TextArea textArea;
+
+    @FXML
+    protected void onStartResult() {
+        welcomeText.setText("Ввод границ области (x1,y1,x1,y2)\nПример (1,4,2,7.4), (6,2.12,4,7.4)...");
     }
 }
